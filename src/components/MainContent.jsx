e3c364f2a237c60c66ace73d3920cb1d0d17d9be
Shaul Friedman <sfriedman@speedata.io>
2022-10-08 15:02:00 +0300

    WIP: MainContent HTML

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,14 @@
 export default function MainContent() {
   return (
-    <nav className="main-content">
-      Here will be main content
-    </nav>
+    <>
+      <h1 className="main-content">Fun facts about React</h1>
+      <ul className="react-facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
   )
 }
